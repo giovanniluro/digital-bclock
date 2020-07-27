55ed1ceac469b09c7c44869d4995b5ac5f03a4e1
Giovanni Luro <giovanni.luro@outlook.com>
2020-07-27 02:24:22 -0300

    Updates

--- precache-manifest.cbfd265baf6f7ef0024ecc9582d18212.js
+++ precache-manifest.cbfd265baf6f7ef0024ecc9582d18212.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5cf4208343aa88605875138cd59661c9",
+    "revision": "68bc121878fb6d177dc32f68aa191b97",
     "url": "/digital-bclock/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/digital-bclock/static/js/2.30e3c53a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "73564035182311334363",
-    "url": "/digital-bclock/static/js/main.6b4a51cf.chunk.js"
+    "revision": "3e4499a9528a9c24afe4",
+    "url": "/digital-bclock/static/js/main.7407a8d0.chunk.js"
   },
   {
     "revision": "63451eba052f54bb20f0",
